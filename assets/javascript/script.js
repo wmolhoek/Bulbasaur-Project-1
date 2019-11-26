3aefec6b5d03e447a3e441ff23fdd7e57af9e07f
wmolhoek <49460886+wmolhoek@users.noreply.github.com>
2019-11-26 11:09:36 -0600

    additional html files and one line of css and js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,10 @@
+$(document).ready(function) {
+    $(".dropdown-trigger").dropdown();
+}
+
+
+
+
 //const game = user input
 const url = "http://www.giantbomb.com/api/search/?api_key=69db5b20e3ad1fdd87c655256f6505e8b020cb8d&format=json&query="+game+"&resources=game"
 
