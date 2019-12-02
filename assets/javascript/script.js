cbfc61181a779b2df965676eedce37c4ce7f9753
Max McCord <maxdmccord@gmail.com>
2019-12-02 15:02:03 -0600

    max-branch-js-movies

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,26 +1,27 @@
-M.AutoInit();
+// M.AutoInit();
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, options);
-    instances.open();
-  });
-
-
-//const game = user input
-const url = "http://www.giantbomb.com/api/search/?api_key=69db5b20e3ad1fdd87c655256f6505e8b020cb8d&format=json&query="+game+"&resources=game"
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.dropdown-trigger');
+//     var instances = M.Dropdown.init(elems, options);
+//     instances.open();
+//   });
 
 
-// api key for OMDB
-const OMDBqueryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+// //const game = user input
+// const url = "http://www.giantbomb.com/api/search/?api_key=69db5b20e3ad1fdd87c655256f6505e8b020cb8d&format=json&query="+game+"&resources=game"
 
 
-// API Key for YouTube = AIzaSyAiMpo3Mnkq2c_KjtywxD7_lxBQTapW0HA
-const youTubeQueryURL = "https://www.googleapis.com/youtube/v3/search";
+// // api key for OMDB
+// const OMDBqueryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
 
 
+// // API Key for YouTube = AIzaSyAiMpo3Mnkq2c_KjtywxD7_lxBQTapW0HA
+// const youTubeQueryURL = "https://www.googleapis.com/youtube/v3/search";
 
-const searchBtn = document.getElementById("button-search");
+
+function search() {
+
+let searchBtn = document.getElementById("button-search");
 const userInput = document.getElementById("user-input");
 
 displayArea = document.getElementById("display-area");
@@ -28,45 +29,45 @@ omdbDisplayArea = document.getElementById("omdb-display");
 
 let moviesArray = [];
 
-function search() {
 
     searchBtn.addEventListener("click", function () {
         let searchTerm = userInput.value;
-
         console.log(searchTerm);
-        let queryURL = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=" + searchTerm + "&api-key=J4LLcLYdwjbGQvjGuEQK9mGgePlM1SWk";
-        // console.log(queryURL);
 
 
-        axios.get(queryURL)
-            .then(function (response) {
-                // console.log(queryURL);
-                console.log(response.data.results);
-                moviesArray = response.data.results;
-                for (i = 0; i < moviesArray.length; i++) {
-                    const movieBtn = document.createElement("a");
-                    movieBtn.innerHTML = moviesArray[i].display_title;
-                    movieBtn.setAttribute("href", moviesArray[i].link.url);
-                    movieBtn.setAttribute("class", "button");
-                    displayArea.append(movieBtn);
+        // axios.get(queryURL)
+        //     .then(function (response) {
+        //         // console.log(queryURL);
+        //         console.log(response.data.results);
+        //         moviesArray = response.data.results;
+        //         for (i = 0; i < moviesArray.length; i++) {
+        //             const movieBtn = document.createElement("a");
+        //             movieBtn.innerHTML = moviesArray[i].display_title;
+        //             movieBtn.setAttribute("href", moviesArray[i].link.url);
+        //             movieBtn.setAttribute("class", "button");
+        //             displayArea.append(movieBtn);
 
 
-                }
+        //         }
 
-            })
+        //     })
         let omdbQueryUrl = "https://www.omdbapi.com/?t=" + searchTerm + "&apikey=trilogy";
         console.log(omdbQueryUrl);
 
         axios.get(omdbQueryUrl)
             .then(function (response) {
-                console.log(response)
+
+                omdbDisplayArea.innerHTML = "";
+                console.log(response);
                 const movieDisplay = document.createElement("div");
                 const titleDisplay = document.createElement("p");
                 const releasedDisplay = document.createElement("p");
                 const ratingIMDBDisplay = document.createElement("p");
                 const ratingRottenTomatoDisplay = document.createElement("p");
                 const ratingMetaCritDisplay = document.createElement("p");
+                const nytReviewBtn = document.createElement("a");
                 const posterDisplay = document.createElement("img");
+
                 console.log(response.data.Title);
                 titleDisplay.innerHTML = "Title: " + response.data.Title;
                 movieDisplay.append(titleDisplay);
@@ -84,8 +85,42 @@ function search() {
                 movieDisplay.append(ratingRottenTomatoDisplay);
 
                 console.log(response.data.Ratings[2].Value);
+                ratingMetaCritDisplay.setAttribute("class", "meta-critic")
                 ratingMetaCritDisplay.innerHTML = "MetaCritic Rating: " + response.data.Ratings[2].Value;
                 movieDisplay.append(ratingMetaCritDisplay);
+
+                const MetaCritic = document.getElementById("meta-critic")
+
+
+
+
+                const movieTitle = response.data.Title;
+
+                let nytReviewURL = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=" + movieTitle + "&api-key=J4LLcLYdwjbGQvjGuEQK9mGgePlM1SWk";
+
+                axios.get(nytReviewURL)
+                    .then(function (response) {
+                        console.log(response);
+                        moviesArray = response.data.results;
+                        console.log(moviesArray);
+
+                        for (i = 0; i < moviesArray.length; i++) {
+                            let nytDisplayTitle = response.data.results[i].display_title;
+                            console.log(nytDisplayTitle);
+                            if (nytDisplayTitle === movieTitle) {
+                                console.log("matched title")
+                                const movieLink = document.createElement("a");
+
+                                movieLink.innerHTML = moviesArray[i].display_title;
+                                movieLink.setAttribute("href", moviesArray[i].link.url);
+                                movieLink.setAttribute("class", "button");
+                                // movieDisplay.append(movieLink);
+                                omdbDisplayArea.prepend(movieLink);
+                            };
+
+                        }
+                    })
+
 
                 posterDisplay.setAttribute("src", response.data.Poster);
                 movieDisplay.append(posterDisplay);
@@ -93,12 +128,13 @@ function search() {
                 omdbDisplayArea.append(movieDisplay);
 
 
-
-
             })
     });
 };
 search()
+
+
+
 
 
 
